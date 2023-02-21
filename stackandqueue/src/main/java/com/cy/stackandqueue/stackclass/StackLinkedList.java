5091d1b50cdb8f556c3653ececdc10f9b95e95f8
Lil-Kr <cxqybox@163.com>
2023-02-22 00:03:35 +0800

    stack for linkedlist

---
+++ stackandqueue/src/main/java/com/cy/stackandqueue/stackclass/StackLinkedList.java
@@ -0,0 +1,42 @@
+package com.cy.stackandqueue.stackclass;
+
+import com.cy.linkedlist.linkedlistclass.LinkedListDummy;
+
+/**
+ * @Author: Lil-K
+ * @Date: 2023/2/21
+ * @Description: 链表实现栈结构
+ */
+public class StackLinkedList<E> implements Stack<E> {
+
+    private LinkedListDummy list;
+
+    public StackLinkedList() {
+        list = new LinkedListDummy();
+    }
+
+    @Override
+    public void push(E e) {
+        list.addLast(e);
+    }
+
+    @Override
+    public E pop() {
+        return (E) list.removeLast();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public E peek() {
+        return (E) list.get(list.getSize() - 1);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+}
